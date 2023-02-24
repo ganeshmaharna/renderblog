ee65c3082a1530f49d5b529d99327786d7da6c76
Ganesh Maharna <maharnaganesh@gmail.com>
2023-02-24 20:26:28 +0530

    Second Commit

--- controller/image-controller.js
+++ controller/image-controller.js
@@ -1,6 +1,6 @@
 import grid from "gridfs-stream";
 import mongoose from "mongoose";
-const url = 'http://localhost:8000'
+const url = ''
 
 let gfs,gridfsBucket;
 const conn = mongoose.connection;
--- server.js
+++ server.js
@@ -17,6 +17,12 @@ app.use(bodyParser.json({extended: true}))
 app.use(bodyParser.urlencoded({extended: true}))
 app.use('/',Router);//here we use the router
 mongoose.set('strictQuery', false);
+if (process.env.NODE_ENV === 'production') {
+    //*Set static folder up in production
+    app.use(express.static('client/build'));
+
+    app.get('*', (req,res) => res.sendFile(path.resolve(__dirname, 'client', 'build','index.html')));
+}
 
 const PORT=process.env.PORT || 8000;
 
